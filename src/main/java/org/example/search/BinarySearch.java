0a2887775fc4142470e05caeabb5716fab6c88ce
JoaoReis <joaovreis015@gmail.com>
2022-11-08 14:18:41 -0300

    first Commit

---
+++ src/main/java/org/example/search/BinarySearch.java
@@ -0,0 +1,11 @@
+package org.example.search;
+
+import java.util.Optional;
+
+public class BinarySearch {
+
+    static Optional<Integer> search(){
+
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/org/example/search/Main.java
@@ -0,0 +1,12 @@
+package org.example.search;
+
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Integer[] orderedList = new Integer[]{12,25,36,85,28,96,47};
+
+        System.out.println(Arrays.toString(orderedList));
+    }
+}
